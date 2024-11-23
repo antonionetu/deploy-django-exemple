7239f8cf8a5f76420c3749e82e7f14e0783d8599
Antonio Fernandes <devantonio.fer@gmail.com>
2024-11-23 00:45:49 -0300

    upload

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Criando ambiente virtual..."
+python -m venv venv
+source venv/bin/activate
+
+echo "Instalando dependências..."
+pip install -r requirements.txt
+
 echo "Migrando banco de dados..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
